522b592f661437d6c4e1a4bb346ef6622d404878
gretaa7x <g.antenucci2@studenti.unimol.it>
2024-05-28 01:40:43 +0200

    chore: Changed App title from the default one to the name of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:bullet_buddy/custom_bottom_bar.dart';
 import 'package:bullet_buddy/pages/create_account_page.dart';
-import 'package:bullet_buddy/pages/home_page.dart';
 import 'package:bullet_buddy/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -17,11 +16,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => const LoginPage(),
         '/create': (context) => const CreateAccountPage(),
-        '/home': (context) => const HomePage(),
         '/bottom': (context) => const CustomBottomBar(),
       },
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Bullet Buddy',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
