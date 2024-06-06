b0b91027325284f542d533564ac7aa5280018bad
gretaa7x <g.antenucci2@studenti.unimol.it>
2024-06-06 16:30:07 +0200

    navigaton: Added new route to named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:bullet_buddy/pages/new_diary_page.dart';
+import 'package:bullet_buddy/pages/page_view_page.dart';
 import 'package:bullet_buddy/widgets/custom_bottom_bar.dart';
 import 'package:bullet_buddy/pages/create_account_page.dart';
 import 'package:bullet_buddy/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MyApp()
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,17 +14,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-        routes: {
-          '/new': (context) => NewDiaryPage(),
-          '/login': (context) => LoginPage(),
-          '/create': (context) => CreateAccountPage(),
-          '/bottom': (context) => const CustomBottomBar(),
-
-        },
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        home: LoginPage(),
-      );
+    return MaterialApp(
+      routes: {
+        '/new': (context) => const NewDiaryPage(),
+        '/view': (context) => const PageViewPage(),
+        '/login': (context) => const LoginPage(),
+        '/create': (context) => const CreateAccountPage(),
+        '/bottom': (context) => const CustomBottomBar(),
+      },
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: const LoginPage(),
+    );
   }
 }
